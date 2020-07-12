15b985d303aff9f38f243b393a0b017cebffb62f
wuwuzula <malin.myemail@gmail.com>
2020-07-12 22:58:42 +0800

    add week 8

---
+++ Week_08/merge-intervals.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[][] merge(int[][] intervals) {
+        int len = intervals.length;
+        if (len < 2) {
+            return intervals;
+        }
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        List<int[]> res = new ArrayList<>();
+        res.add(intervals[0]);
+        for (int i = 1; i < len; i++) {
+            int[] curInterval = intervals[i];
+            int[] peek = res.get(res.size() - 1);
+            if (curInterval[0] > peek[1]) {
+                res.add(curInterval);
+            } else {
+                peek[1] = Math.max(curInterval[1], peek[1]);
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
+
